56a00529d7e5fc5fedc1471698190f5ef8fdb58d
SE Zeltmann <steven.zeltmann@lbl.gov>
2023-01-27 16:22:25 -0500

    delete old stuff

--- old/py4D_browser/__init__.py
+++
@@ -1,3 +0,0 @@
-from py4D_browser.dialogs import *
-from py4D_browser.viewer import *
-from py4D_browser.gui_utils import *
--- old/py4D_browser/strain/__init__.py
+++
@@ -1,3 +0,0 @@
-from py4D_browser.strain.strain_window import *
-from py4D_browser.strain.panels import *
-from py4D_browser.strain.ImageViewMasked import *
