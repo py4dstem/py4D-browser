b221d947ec2001e3f6918d010404fb1b48000975
SE Zeltmann <steven.zeltmann@lbl.gov>
2023-01-27 16:36:32 -0500

    version

--- setup.py
+++ setup.py
@@ -4,14 +4,9 @@ from distutils.util import convert_path
 with open("README.md","r") as f:
     long_description = f.read()
 
-version_ns = {}
-vpath = convert_path('py4D_browser/version.py')
-with open(vpath) as version_file:
-    exec(version_file.read(), version_ns)
-
 setup(
     name='py4D_browser',
-    version=version_ns['__version__'],
+    version="0.9",
     packages=find_packages(),
     description='A 4D-STEM data browser built on py4DSTEM.',
     long_description=long_description,
@@ -21,7 +16,7 @@ setup(
     author_email='steven.zeltmann@lbl.gov',
     license='GNU GPLv3',
     keywords="STEM 4DSTEM",
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[
         'py4dstem >= 0.13.11',
         'numpy >= 1.19',
