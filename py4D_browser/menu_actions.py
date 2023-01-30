56af5a2f84494cdd2f994662c86ef66ee3b9dc60
SE Zeltmann <steven.zeltmann@lbl.gov>
2023-01-30 13:16:02 -0500

    basic support for finding 4D data in HDF5 files (complex files not tested)

--- py4D_browser/menu_actions.py
+++ py4D_browser/menu_actions.py
@@ -1,5 +1,6 @@
 import py4DSTEM
 from PyQt5.QtWidgets import QFileDialog
+import h5py
 
 
 def load_data_auto(self):
@@ -21,11 +22,17 @@ def load_data_bin(self):
 def load_file(self, filepath, mmap=False, binning=1):
     print(f"Loading file {filepath}")
 
-    self.datacube = py4DSTEM.import_file(
-        filepath,
-        mem="MEMMAP" if mmap else "RAM",
-        binfactor=binning,
-    )
+    if py4DSTEM.io.utils.parse_filetype(filepath) == "py4DSTEM":
+        datacubes = get_4D(h5py.File(filepath, "r"))
+        if len(datacubes) >= 1:
+            # Read the first datacube in the HDF5 file into RAM
+            self.datacube = py4DSTEM.io.DataCube(datacubes[0][()])
+    else:
+        self.datacube = py4DSTEM.import_file(
+            filepath,
+            mem="MEMMAP" if mmap else "RAM",
+            binfactor=binning,
+        )
 
     self.update_diffraction_space_view(reset=True)
     self.update_real_space_view(reset=True)
@@ -45,3 +52,16 @@ def show_file_dialog(self):
     else:
         print("File was invalid, or something?")
         raise ValueError("Could not read file")
+
+
+def get_4D(f, datacubes=None):
+    if datacubes is None:
+        datacubes = []
+    for k in f.keys():
+        if isinstance(f[k], h5py.Dataset):
+            # we found data
+            if len(f[k].shape) == 4:
+                datacubes.append(f[k])
+        elif isinstance(f[k], h5py.Group):
+            get_4D(f[k], datacubes)
+    return datacubes
