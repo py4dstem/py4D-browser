446fbe3c7fe901a55b3482f3f610f493dff46576
Steven Zeltmann <steven.zeltmann@cornell.edu>
2026-04-20 16:14:36 -0400

    postinit mechanics for plugins

--- src/py4D_browser/plugins.py
+++ src/py4D_browser/plugins.py
@@ -71,11 +71,18 @@ def load_plugins(self):
                             ),
                             "menu": plugin_menu,
                             "action": plugin_action,
+                            "id": plugin_id,
                         }
                     )
                 except Exception as exc:
                     print(f"Failed to load plugin.\n{exc}")
                     print(traceback.print_exc())
+
+        # run post-initialization so that plugins can see all other loaded plugins
+        for plugin_dict in self.loaded_plugins:
+            plugin = plugin_dict["plugin"]
+            if hasattr(plugin, "post_init"):
+                plugin.post_init(parent=self)
 
 
 def unload_plugins(self):
@@ -84,7 +91,7 @@ def unload_plugins(self):
         plugin["plugin"].close()
 
 
-class ExamplePlugin:
+class py4DBrowserPlugin:
 
     # required for py4DGUI to recognize this as a plugin.
     plugin_id = "my.plugin.identifier"
@@ -104,6 +111,12 @@ class ExamplePlugin:
 
     def __init__(self, parent, **kwargs):
         self.parent = parent
+
+    def post_init(self, parent, **kwargs):
+        # This is called after *all* plugins are loaded and __init__'ed
+        # to enabled to plugins to discover and hook into one another.
+        # ADDED IN v1.5.0 (currently called only with parent argument)
+        pass
 
     def close(self):
         pass  # perform any shutdown activities
